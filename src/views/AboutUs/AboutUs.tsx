18f61ddc1ead44b496975848af6432b2cc283826
faraz-dhillon <faraz.tacticminds@gmail.com>
2026-05-23 23:49:02 +0500

    about us sectio

--- src/views/AboutUs/AboutUs.tsx
+++ src/views/AboutUs/AboutUs.tsx
@@ -1,13 +1,14 @@
-import { Box } from '@/components/elements';
 import DashBoardHeader from '@/components/ui/DashBoardHeader/DashBoardHeader';
+import { AboutWrapper } from './style';
 
 export const AboutUs = () => {
   return (
-    <Box>
+    <AboutWrapper>
       <DashBoardHeader
         heading="About Pet Connect"
         subHeading="Learn more about our story, mission, and the people behind PetConnect."
       />
-    </Box>
+
+    </AboutWrapper>
   );
 };
